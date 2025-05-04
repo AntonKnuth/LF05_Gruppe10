7ebd84b5f03db39244d3b29db9bb91b6776415a2
Oleg_B <anton.knuth@t-online.de>
2025-05-04 21:45:58 +0200

    - unnötige_Dateien

--- test_abfrage_fuer_zutat_ernaehrungskategorie.sql
+++
@@ -1,4 +0,0 @@
-SELECT z.Bezeichnung, k.ErnaehrungkategorieTyp
-FROM zutat z
-JOIN zutat_ernaehrungskategorie z_k ON z.ZutatId = z_k.ZutatId
-JOIN ernaehrungskategorie k ON z_k.ErnaehrungskategorieId = k.ErnaehrungskategorieId
